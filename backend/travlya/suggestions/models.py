fa8547ed0aff78eba77360ea98c4189ad095fb7a
swingkiddo <yoshibeatsu@gmail.com>
2024-03-05 20:34:47 +0300

    add display_name and profile_image to TwitchUser model

--- backend/travlya/suggestions/models.py
+++ backend/travlya/suggestions/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class TwitchUser(models.Model):
     twitch_id = models.PositiveIntegerField(primary_key=True)
     username = models.CharField(max_length=30)
+    display_name = models.CharField(max_length=50)
     rating = models.IntegerField()
+    profile_image = models.CharField(max_length=50)
 
 class Suggestion(models.Model):
     user = models.ForeignKey(TwitchUser, on_delete=models.CASCADE, related_name="suggestions")
