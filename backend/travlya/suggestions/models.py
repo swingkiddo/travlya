7b7dce01b81d770e68ada637b73ee75d399623a9
swingkiddo <yoshibeatsu@gmail.com>
2024-03-07 00:27:17 +0300

    update TwitchUser and Suggestion models

--- backend/travlya/suggestions/models.py
+++ backend/travlya/suggestions/models.py
@@ -5,9 +5,10 @@ class TwitchUser(models.Model):
     username = models.CharField(max_length=30)
     display_name = models.CharField(max_length=50)
     rating = models.IntegerField()
-    profile_image = models.CharField(max_length=50)
+    profile_image = models.CharField(max_length=200)
 
 class Suggestion(models.Model):
     user = models.ForeignKey(TwitchUser, on_delete=models.CASCADE, related_name="suggestions")
-    url = models.CharField(max_length=100)
+    url = models.CharField(max_length=100, unique=True)
+    embed_url = models.CharField(max_length=100, unique=True)
     timestamp = models.DateTimeField(auto_now_add=True)
