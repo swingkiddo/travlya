d92ebcb0082819804d78ed52dd6604f02e43435e
swingkiddo <yoshibeatsu@gmail.com>
2024-03-17 01:32:36 +0300

    bot update

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,12 @@
-from twitchio import Message, User
+from twitchio import Message, User, Chatter
 from twitchio.ext import commands
 from datetime import datetime
 from dateutil import parser
 import config
 import aiohttp
-# import requests
+import requests
 import logging
+import json
 
 logger = logging.Logger("bot.logs", level=logging.DEBUG)
 
@@ -35,9 +36,9 @@ def whisper_to_user(text, from_id, user_id):
 def is_admin(id):
     return True if id in ADMIN_IDS else False
 
-class User:
-    def __init__(self, twitch_id: int, username: str):
-        pass
+# class User:
+#     def __init__(self, twitch_id: int, username: str):
+#         pass
         
 class Suggestion:
     def __init__(self, url: str, user: User, timestamp: datetime):
@@ -78,12 +79,26 @@ class Bot(commands.Bot):
                     users = await res.json()
                     return users
                 
-    async def register_user(self, data):
+    async def register_user(self, user: User):
         url = f"{API_URL}/users"
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, data=data) as res:
-                if res.status == 201:
-                    pass
+        users = await self.fetch_users(ids=[user.id])
+        if users:
+            user = users[0]
+            data = {
+                "twitch_id": user.id,
+                "username": user.name,
+                "display_name": user.display_name,
+                "profile_image": user.profile_image,
+                "rating": 0
+            }
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, data=data) as res:
+                    if res.status == 201:
+                        print(f"user {user.name} registered")
+                        self.users.append(user.id)
+                    else:
+                        print(await res.content.read())
+                        print(res.reason)
 
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
@@ -91,6 +106,8 @@ class Bot(commands.Bot):
         users = await self.get_users()
         self.users = [u["twitch_id"] for u in users]
         self.suggestions = {u["twitch_id"]: u["suggestions"] for u in users}
+        print(f"{self.users=}")
+        print(f"{self.suggestions=}")
 
     def timeout_user(self, channel_id, moderator_id, user_id):
         print(self.timeouts)
@@ -112,6 +129,13 @@ class Bot(commands.Bot):
                 "reason": "no reason"
             }
         }
+        # async with aiohttp.ClientSession() as session:
+        #     async with session.post(url, data=data) as res:
+        #         if res.status != 200:
+        #             print("Не удалось отстранить пользователя")
+        #             logger.debug(res.text)
+        #         print(res.status_code)
+        #         print(res.content)
         res = requests.post(url, headers=headers, json=data)
         
         if res.status_code != 200:
@@ -147,6 +171,8 @@ class Bot(commands.Bot):
             return
         
         author = message.author
+        if int(author.id) not in self.users:
+            await self.register_user(author)
         content = message.content.strip()
         command = content.split(" ")[0]
         channel = self.connected_channels[0]
@@ -170,31 +196,55 @@ class Bot(commands.Bot):
                     splitted = message.content.split(" ")
                     if len(splitted) > 1:
                         link = get_link_from_message(content)
+                        print(f"{link=}")
                         if self.link_already_added(link):
+                            if self.link_added_by_user(link, author):
                                 self.delete_message(channel_owner.id, self.user_id, message.id, author.id)
+                            else:
+                                await self.connected_channels[0].send(f"@{author.name}, ссылка уже предложена другим пользователем")
                         else:
                             await self.add_suggestion(author, link)
     
-    async def add_suggestion(self, author: User, suggestion: str):
-        if not author.id in self.suggestions:
-            self.suggestions[author.id] = [suggestion]
-        else:
-            # for s in suggestions:
-                # if s in suggestion:
-                #     self.
-            self.suggestions[author.id].append(suggestion)
-        print(self.suggestions)
-        text = f"@{author.name}, ссылка добавлена"
-        await self.connected_channels[0].send(text)
+    async def add_suggestion(self, author: Chatter, suggestion: str):
+        url = f"{API_URL}/suggestions"
+        embed = suggestion.replace("watch?v=", "embed/")
+        data = {
+            "user": author.id,
+            "url": suggestion,
+            "embed_url": embed
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, data=data) as res:
+                if res.status == 201:
+                    content = await res.content.read()
+                    new_suggestion = json.loads(content)
+                    user_id = int(author.id)
+                    if not user_id in self.suggestions:
+                        self.suggestions[user_id] = [new_suggestion]
+                    else:
+                        self.suggestions[user_id].append(new_suggestion)
+                    print(self.suggestions)
+                    text = f"@{author.name}, ссылка добавлена"
+                    await self.connected_channels[0].send(text)
+                else:
+                    print(await res.content.read())
 
         # await self.handle_commands(message)
 
     def link_already_added(self, link: str) -> bool:
-        suggestions = []
-        for s in self.suggestions.values():
-            suggestions.append(*s)
-        if link in suggestions:
+        added_links = []
+        for suggestion_list in self.suggestions.values():
+            added_links += [s["url"] for s in suggestion_list]
+        if link in added_links:
             return True
+        return False
+
+    def link_added_by_user(self, link: str, user: Chatter):
+        user_id = int(user.id)
+        if user_id in self.suggestions:
+            user_links = [sug["url"] for sug in self.suggestions[user_id]]
+            if link in user_links:
+                return True
         return False
 
     @commands.command()
